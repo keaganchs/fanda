b476d03bcf7613436dc278d35b6f5df58c342ff2
Keagan Holmes <holmes@steinbuch.tail51ec2d.ts.net>
2026-06-05 12:32:37 +0200

    Update plot

--- trwm/newt_xl_dyn.py
+++ trwm/newt_xl_dyn.py
@@ -1,6 +1,7 @@
 import wandb
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from fanda.wandb_client import fetch_wandb
 from fanda import transforms
@@ -13,7 +14,7 @@ np.random.seed(42)
 
 
 df = (
-    fetch_wandb("trm-dynamics", "TRM Dynamics", filters={"group": {"$regex": ".*-newt-S-(128ld|512ld|baseline)$"}})
+    fetch_wandb("trm-dynamics", "TRM Dynamics", filters={"group": {"$regex": ".*-newt-S-(16ld|128ld|512ld|baseline)$"}})
     # .pipe(transforms.truncate, column="eval/episode_reward", groupby="group")
     # .pipe(
     #     transforms.remove_outliers,
@@ -26,14 +27,25 @@ df = (
 
 # Add legend column for the last part of the group name after the last dash
 df["legend"] = df["group"].apply(lambda x: x.split("-")[-1])
+df["legend"] = df["legend"].replace({"baseline": "384ld (baseline)"})
+
+
+palette = {
+    "16ld": "tab:blue",
+    "128ld": "tab:orange",
+    "384ld (baseline)": "tab:green",
+    "512ld": "tab:gray",
+}
+labels = list(palette.keys())
 
 fanda = (
     lineplot(
         df=df,
+        figsize=(10.24, 10.24),
         x="eval/step",
         y="eval/episode_reward",
-        hue="group",
-        palette="deep",
+        hue="legend",
+        palette=palette,
         errorbar="sd",
         err_kws={"alpha": 0.15},
     )
@@ -49,9 +61,11 @@ fanda = (
     )
     .pipe(
         add_legend,
-        labels=df["legend"].unique(),
+        labels=labels,
+        palette=palette,
         fontsize="xx-large",
     )
+    .pipe(lambda f: [plt.tight_layout(), f][1])
     .pipe(show_fig)
     .pipe(save_fig, name="images/noisy_training", format="png")
     .pipe(close_fig)
